ae37594f5da5ac5fa176fd6a12cb8c5fed05598c
celvro <dwlfk2@mst.edu>
2015-04-28 12:44:49 -0500

    Prob4, now calculates numbers instead of using precomputed list

---
+++ Problem4/prob4.py
@@ -0,0 +1,25 @@
+from math import sqrt
+import sys
+
+def vampire(a,b):
+    return sorted(str(a*b)) == sorted(str(a)+str(b))
+    
+MAX = 1000300
+
+x = set()
+for i in range(1, MAX):
+    for j in range(i, MAX//i):
+        if vampire(i,j):
+            x.add(i*j)
+x = sorted(x)
+
+n = int(raw_input())
+
+while n:
+    for i in x:
+        if i>=n:
+            print i
+            n = int(raw_input())
+            break
+    print "Not found"
+    break
